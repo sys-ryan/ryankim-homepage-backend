29e6e4f2d61e22cc50c8750bfb8dfe80e3137c17
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-10-12 03:57:55 +0900

    feat: create post API

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -1,9 +1,13 @@
-import { Module } from '@nestjs/common';
-import { CategoriesService } from './categories.service';
-import { CategoriesController } from './categories.controller';
+import { Module } from "@nestjs/common";
+import { CategoriesService } from "./categories.service";
+import { CategoriesController } from "./categories.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Categories } from "./entities/categories.entity";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Categories])],
   controllers: [CategoriesController],
-  providers: [CategoriesService]
+  providers: [CategoriesService],
+  exports: [CategoriesService],
 })
 export class CategoriesModule {}
