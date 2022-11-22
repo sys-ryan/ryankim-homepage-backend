f72676d4568f1a70b7bad9eeaa602aa531b964bc
Ryan Kim <ryankim@Ryanui-MacBookPro.local>
2022-11-23 01:48:49 +0900

    feat: create post dto 수정

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,18 +1,23 @@
-import { IsNumber, IsString, IsUrl } from "class-validator";
+import { IsEnum, IsNumber, IsString, IsUrl } from "class-validator";
+import { POST_CATEGORY } from "src/common/enums/post-category.enum";
+import { POST_SUBCATEGORY } from "src/common/enums/post-subCategory.enum";
 
 export class CreatePostDto {
   @IsString()
   title: string;
 
-  @IsString()
+  @IsEnum(POST_CATEGORY)
   category: string;
 
   @IsUrl()
   thumbnail: string;
 
   @IsString()
-  excerpt: string;
+  markdown: string;
 
   @IsString()
+  excerpt: string;
+
+  @IsEnum(POST_SUBCATEGORY)
   subCategory: string;
 }
